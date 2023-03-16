c29f7157b33c3ed7764f1ad493b16385db54461a
lihuashiyu <lihuashiyu@github.com>
2023-03-16 12:38:05 +0800

    添加编译打包脚本

--- deploy/shell/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SERVICE_DIR=$(cd "$(dirname "$0")/../" || exit; pwd)       # 程序位置
-SERVICE_NAME=real-time                                     # 程序名称
-ALIAS_NAME=RealTime                                        # 程序别名
-LOG_FILE=process-$(date +%F).log                           # 程序运行日志文件
-
